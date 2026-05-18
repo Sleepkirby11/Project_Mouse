6cddd00993f2b54065d451a74723748216954fb9
Sleepkirby11 <seowon7005@gmail.com>
2026-05-19 02:04:21 +0900

    05.19_플레이어 시스템 추가
    스킬과 일반 공격 분리 + 커서 이동 범위 설정
    =일부 로직 보완
    +공격류 오브젝트가 실제 대미지를 입히도록 각 스크립트에 OnTriggerEnter2D 추가

---
+++ Project/Assets/Script/SW/AttackCursor.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AttackCursor : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    //마우스 관련
+    Vector2 mouse;
+    public Transform target;
+    float distance;
+    float range = 15f;
+
+    //초기화
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //target Null체크
+        if(target != null)
+        {
+            //mouse, distance 업데이트
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            distance = Vector2.Distance(target.position, mouse);
+        }
+
+        //AttackCursor의 position 최대 값 range로 지정
+        if (distance > range)
+            rigid.position = (Vector2)(target.position) + (mouse.normalized * range);
+        else
+            rigid.position = (Vector2)mouse;
+    }
+}
--- Project/Assets/Script/SW/NormalAttack.cs
+++ Project/Assets/Script/SW/NormalAttack.cs
@@ -1,18 +1,89 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NormalAttack : MonoBehaviour
 {
-    Rigidbody2D rigid;
-    Vector2 mouse;
+    //Trail의 지속시간
+    public float lifeTime;
+
+    //드로잉 가능 여부
+    public bool isMove;
+
+    TrailRenderer trail;
+    EdgeCollider2D col;
+
+    //Trail의 정점 변수
+    List<Vector2> points = new List<Vector2>();
+
+    //마우스 좌표 저장
+    public Transform mouse;
+
+    public int damage;
+
+
+    //초기화
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        trail = GetComponent<TrailRenderer>();
+        col = GetComponent<EdgeCollider2D>();
+        trail.time = 9999;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rigid.position = mouse;
+        //endWidth는 항상 startWidth와 동일시
+        trail.endWidth = trail.startWidth;
+
+        //지속 시간에 따른 trail의 크기 변화 및 collider의 enabled 여부
+        if (lifeTime > 0)
+        {
+            col.enabled = true;
+
+            lifeTime -= Time.deltaTime;
+            trail.startWidth = lifeTime * 2;
+        }
+        else
+        {
+            col.enabled = false;
+        }
+
+        //그리는 중 마우스 좌표 따라 이동
+        if (isMove)
+        {
+            transform.position = mouse.transform.position;
+        }
+
+
+    }
+
+    //Trail에 Collider를 입히는 과정
+    public void SetColliderPointsFromTrail()
+    {
+        // 트레일의 정점 개수(2개 이상) 확인
+        int positionCount = trail.positionCount;
+        if (positionCount < 2) return;
+
+        Vector3[] trailPositions = new Vector3[positionCount];
+        trail.GetPositions(trailPositions);
+
+        points.Clear();
+
+        // 트레일 좌표를 로컬 좌표로 변환 후 할당
+        for (int i = 0; i < positionCount; i++)
+        {
+            points.Add(transform.InverseTransformPoint(trailPositions[i]));
+        }
+
+        col.points = points.ToArray();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyStatus>().TakeDamage(damage);
+        }
     }
 }
+
