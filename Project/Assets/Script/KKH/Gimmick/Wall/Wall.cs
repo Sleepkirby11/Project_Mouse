7dfdffa1ffb0d8a3dea805ff1f316b0bfbc4e575
parknayun <kkh39088@gmail.com>
2026-05-19 11:29:30 +0900

    레버, 벽
    상호작용을 담당하는 스크립트와 레버, 벽 만들었습니다 나중에 플레이어 담당이 '플레이어가 지정한 상호작용(사용자설정)키'를 입력하면 IInteractable.cs의 Interact함수 실행시키면 됩니다

---
+++ Project/Assets/Script/KKH/Gimmick/Wall/Wall.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public void Collapse()
+    {
+        Debug.Log($"{gameObject.name} 벽이 파괴되었습니다.");
+        Destroy(gameObject);
+    }
+}
