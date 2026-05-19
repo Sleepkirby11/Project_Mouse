2f0fa9858b8372039636327d0ee46a492f81b877
siu0526 <amita050526@gmail.com>
2026-05-19 16:44:29 +0900

    KSW UI 배치 및 게임 시작 종료 함수 제작

---
+++ Project/Assets/Script/KSW/UI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 이동
+
+public class UI : MonoBehaviour
+{
+    // 게임 시작 버튼 누를 때 호출
+    public void StartGame()
+    {
+        SceneManager.LoadScene(""); //시작 씬 이름 넣을 것
+    }
+
+
+
+    // 종료 버튼 누를 때 호출
+    public void ExitGame()
+    {
+        Debug.Log("게임 종료!"); // 에디터에서는 안 꺼지므로 로그로 확인
+        Application.Quit();     // 실제 빌드된 게임에서는 프로그램 종료
+    }
+}
