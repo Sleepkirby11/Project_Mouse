14d33126c9b31e521e64250db9cd4698258cea41
Simyesung <randeur02@gmail.com>
2026-06-08 13:40:01 +0900

    보스 바람 추가

---
+++ Project/Assets/Script/ACY/Bullet/Wind.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [Header("바람 연출 설정")]
+    [SerializeField] private float speed = 5f;        
+    [SerializeField] private float lifeTime = 1.5f;    
+
+    private const string POOL_KEY = "BossWindBlast";
+    private float currentLifeTime;
+
+    private void OnEnable()
+    {
+        currentLifeTime = lifeTime;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        currentLifeTime -= Time.deltaTime;
+        if (currentLifeTime <= 0f)
+        {
+            PoolingManager.Instance.Return(POOL_KEY, gameObject);
+        }
+    }
+}
