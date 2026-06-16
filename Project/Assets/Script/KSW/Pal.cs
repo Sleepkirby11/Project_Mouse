6140edcc8fa257855a797de01846e8092dd2eb91
siu0526 <amita050526@gmail.com>
2026-06-16 20:17:48 +0900

    색 바꾸기

---
+++ Project/Assets/Script/KSW/Pal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pal : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public int stateNum;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UI.Instance.ChangeStance(stateNum);
+    }
+}
