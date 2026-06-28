9f2af47d9853f46b2c85d6d7932c8844e5d71ae7
acy <skata567@naver.com>
2026-06-28 15:45:21 +0900

    123

---
+++ Project/Assets/Script/KSW/GameClear.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour
+{
+    public string titleSceneName = "KSW_Scene";
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene(titleSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("게임 종료!");
+        Application.Quit();
+    }
+}
