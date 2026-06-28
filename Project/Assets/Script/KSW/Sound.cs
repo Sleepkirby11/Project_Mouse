d6f88c2412b816589d4eedb00e2b4c8fec001242
siu0526 <amita050526@gmail.com>
2026-06-28 15:32:19 +0900

    버튼 버그 해결 + 클리어 씬 + 라이선스 창 임시

---
+++ Project/Assets/Script/KSW/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // 슬라이더와 이미지를 쓰기 위해 필요
+
+public class SoundControl : MonoBehaviour
+{
+    public Slider volumeSlider;    
+    public Image volumeIcon;       
+
+    public Sprite soundOnSprite;   
+    public Sprite soundMuteSprite; 
+
+    void Start()
+    {
+        volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+
+        OnVolumeChanged(volumeSlider.value);
+    }
+
+    void OnVolumeChanged(float value)
+    {
+        if (value <= 0)
+        {
+            volumeIcon.sprite = soundMuteSprite;
+        }
+        else
+        {
+            volumeIcon.sprite = soundOnSprite;
+        }
+    }
+}
