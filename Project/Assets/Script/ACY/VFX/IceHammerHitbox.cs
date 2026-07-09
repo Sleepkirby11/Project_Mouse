6b3770743611cbd0e154389a3f80bdf0d0f7c753
simyesung <sys031123aass1004@gmail.com>
2026-07-09 10:00:08 +0900

    해머 판정 추가

--- Project/Assets/Script/ACY/VFX/IceHammerHitbox.cs
+++ Project/Assets/Script/ACY/VFX/IceHammerHitbox.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class HitboxProxy : MonoBehaviour, IDamageable
 {
@@ -14,6 +14,24 @@ public class HitboxProxy : MonoBehaviour, IDamageable
         if (parentHammer != null)
         {
             parentHammer.TakeDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"[HitboxProxy] OnTriggerEnter2D with {collision.gameObject.name}");
+        if (parentHammer != null)
+        {
+            parentHammer.HandlePlayerCollision(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"[HitboxProxy] OnCollisionEnter2D with {collision.gameObject.name}");
+        if (parentHammer != null)
+        {
+            parentHammer.HandlePlayerCollision(collision.gameObject);
         }
     }
 }
